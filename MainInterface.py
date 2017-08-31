c1c902997d5a4cece8eecb8acd7f8721952aeeb2
jakobliggett <jakobliggett@gmail.com>
2017-08-31 14:51:51 -0400

    Working on showing board 2

--- MainInterface.py
+++ MainInterface.py
@@ -16,8 +16,28 @@ board = [
     [],
 ]
 
+num2letter = {'a':0, 'b':1, 'c':2, 'd':3, 'e':4, 'f':5, 'g':6, 'h':7, 'i':8, 'j':9}
+
 for _ in board:
     _.append(' . '*10)
+
+def check_input(teststr):
+    ##Checks if an input if valid, if so returns it, if not returns 'None'
+    valid = False
+    while not valid:
+        str = input(teststr)
+        try:
+            if not str[0].isalpha():
+                raise Exception
+            if not len(str) == 2:
+                raise Exception
+            test_int = int(str[1])
+
+            ##String is good
+            valid = True
+            return num2letter[0]+str[1]
+        except:
+            print('Invalid Input! \n')
 
 def show_board(board_in):
     for row in board_in:
@@ -28,6 +48,7 @@ def main():
     show_board(board)
     game_ended = False
     while not game_ended:
-        print(input('Guess: '))
+        print(check_input('Guess: '))
+        #print('test')
 
 main()
