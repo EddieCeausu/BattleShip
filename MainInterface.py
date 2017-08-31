6a08dfa8854281764142335b9d9ff51852b9e886
jakobliggett <jakobliggett@gmail.com>
2017-08-31 14:24:20 -0400

    Commit test

--- MainInterface.py
+++ MainInterface.py
@@ -1,3 +1,23 @@
+board = [
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+]
+
+for row in board:
+    row.append([False]*10)
+
 def main():
     print('Welcome to battleship v0.01!')
-    ##Test
+    for row in board:
+        for value in row:
+            #print(value, end='')
+
+main()
